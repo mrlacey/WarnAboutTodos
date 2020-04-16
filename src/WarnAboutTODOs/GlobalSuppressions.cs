cabae8b6c52878c328b680676f2fe98a84982902
Matt Lacey <matt@mrlacey.co.uk>
2020-04-16 13:55:26 +0100

    hide documentation warning

---
+++ src/WarnAboutTODOs/GlobalSuppressions.cs
@@ -0,0 +1,7 @@
+﻿// <copyright file="GlobalSuppressions.cs" company="Matt Lacey Ltd.">
+// Copyright (c) Matt Lacey Ltd. All rights reserved.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1652:Enable XML documentation output", Justification = "Documentation not needed.")]
